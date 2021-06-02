689d1ef61ce3b24ec5d94b54ec958999c0289290
CindoddCindy <cindodcindy@gmail.com>
2021-06-02 11:11:05 +0700

    tambah model di tiga kelas

--- etherapyproject/src/main/java/com/etherapy/etherapyproject/EtherapyprojectApplication.java
+++ etherapyproject/src/main/java/com/etherapy/etherapyproject/EtherapyprojectApplication.java
@@ -2,8 +2,10 @@ package com.etherapy.etherapyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EtherapyprojectApplication {
 
 	public static void main(String[] args) {
--- etherapyproject/src/main/java/com/etherapy/etherapyproject/model/Kristen.java
+++ etherapyproject/src/main/java/com/etherapy/etherapyproject/model/Kristen.java
@@ -1,4 +1,61 @@
 package com.etherapy.etherapyproject.model;
 
-public class Kristen {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "kristen")
+public class Kristen extends AuditModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String title;
+
+    @NotNull
+    @Size(max = 250)
+    private String description;
+
+    @NotNull
+    private String content;
+
+    public Kristen() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
