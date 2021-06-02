eee3a2ac1367908f5eee4117b875e7fe220544a0
CindoddCindy <cindodcindy@gmail.com>
2021-06-02 15:22:40 +0700

    tambah model user dan model admin

--- etherapyproject/src/main/java/com/etherapy/etherapyproject/model/Admin.java
+++ etherapyproject/src/main/java/com/etherapy/etherapyproject/model/Admin.java
@@ -1,4 +1,61 @@
 package com.etherapy.etherapyproject.model;
 
-public class Admin {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "admin")
+public class Admin extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String nama;
+
+    @NotNull
+    @Size(max = 250)
+    private String email;
+
+    @NotNull
+    @Size(max = 250)
+    private String phone;
+
+    public Admin() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- etherapyproject/src/main/java/com/etherapy/etherapyproject/model/User.java
+++ etherapyproject/src/main/java/com/etherapy/etherapyproject/model/User.java
@@ -1,4 +1,62 @@
 package com.etherapy.etherapyproject.model;
 
-public class User {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String nama;
+
+    @NotNull
+    @Size(max = 250)
+    private String email;
+
+    @NotNull
+    @Size(max = 250)
+    private String phone;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
