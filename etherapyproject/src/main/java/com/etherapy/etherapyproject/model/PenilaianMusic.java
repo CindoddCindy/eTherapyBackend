035aaf511968effa61424babdf7cb471a39ecc7c
CindoddCindy <cindodcindy@gmail.com>
2021-06-05 19:34:42 +0700

    tambah penilaian

--- etherapyproject/src/main/java/com/etherapy/etherapyproject/model/PenilaianMusic.java
+++ etherapyproject/src/main/java/com/etherapy/etherapyproject/model/PenilaianMusic.java
@@ -1,5 +1,52 @@
 package com.etherapy.etherapyproject.model;
 
-public class PenilaianMusic
-{
+import com.etherapy.etherapyproject.music.model.Music;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "penilaianmusic")
+public class PenilaianMusic extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotNull
+    private String rate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "music_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Music music;
+
+    public PenilaianMusic() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 }
--- etherapyproject/src/main/java/com/etherapy/etherapyproject/repository/PenilaianMusicRepository.java
+++ etherapyproject/src/main/java/com/etherapy/etherapyproject/repository/PenilaianMusicRepository.java
@@ -1,4 +1,13 @@
 package com.etherapy.etherapyproject.repository;
 
-public interface PenilaianMusicRepository {
+import com.etherapy.etherapyproject.model.PenilaianMusic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PenilaianMusicRepository extends JpaRepository<PenilaianMusic, String> {
+    Page<PenilaianMusic> findByMusicId(String musicId, Pageable pageable);
+    Optional<PenilaianMusic> findByIdAndMusicId(String id, String musicId);
 }
